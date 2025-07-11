73273dad469ececa361e04771bff34499da2b464
KasumiMercury <naoki_shira@outlook.jp>
2025-07-11 14:56:54 +0900

    refactor

--- internal/infra/handler/task.go
+++ internal/infra/handler/task.go
@@ -1,10 +1,8 @@
 package handler
 
 import (
-	"context"
 	"fmt"
 	"github.com/KasumiMercury/todo-server-poc-go/internal/controller"
-	"github.com/KasumiMercury/todo-server-poc-go/internal/domain"
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,36 +18,50 @@ func NewTask(ctr controller.Task) *Task {
 
 func (t *Task) Register(r *gin.Engine) {
 	r.GET("/task/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		task, err := t.GetTaskById(c.Request.Context(), id)
-		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
+		if err := t.GetTaskById(c); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
 			return
 		}
-		fmt.Println(task)
-
-		res := struct {
-			Id    string `json:"id"`
-			Title string `json:"title"`
-		}{
-			Id:    task.ID(),
-			Title: task.Title(),
-		}
-
-		c.JSON(200, res)
 	})
 
 }
 
-func (t *Task) GetTaskById(ctx context.Context, id string) (domain.Task, error) {
+func (t *Task) GetTaskById(c *gin.Context) error {
+	id := c.Param("id")
 	if id == "" {
-		panic("ID must not be empty")
+		return fmt.Errorf("ID must not be empty")
 	}
 
-	task, err := t.ctr.GetTaskById(ctx, id)
+	task, err := t.ctr.GetTaskById(c.Request.Context(), id)
 	if err != nil {
-		return domain.Task{}, err
+		return fmt.Errorf("failed to get task by ID: %w", err)
 	}
 
-	return *task, nil
+	if task == nil {
+		return fmt.Errorf("task not found")
+	}
+
+	res := struct {
+		Id    string `json:"id"`
+		Title string `json:"title"`
+	}{
+		Id:    task.ID(),
+		Title: task.Title(),
+	}
+
+	c.JSON(200, res)
+	return nil
 }
+
+//func (t *Task) GetTaskById(ctx context.Context, id string) (domain.Task, error) {
+//	if id == "" {
+//		panic("ID must not be empty")
+//	}
+//
+//	task, err := t.ctr.GetTaskById(ctx, id)
+//	if err != nil {
+//		return domain.Task{}, err
+//	}
+//
+//	return *task, nil
+//}
