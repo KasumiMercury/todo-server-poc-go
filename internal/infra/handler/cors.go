d3fa8d55dae2c0dcaf536a15641ca3a8c03180df
KasumiMercury <naoki_shira@outlook.jp>
2025-07-14 16:17:01 +0900

    fix: fix cors setting

--- internal/infra/handler/cors.go
+++ internal/infra/handler/cors.go
@@ -7,7 +7,10 @@ import (
 // CORSMiddleware returns a Gin middleware that handles CORS headers
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://localhost:5173")
+		origin := c.Request.Header.Get("Origin")
+		if origin == "http://localhost:5173" || origin == "http://localhost:3000" {
+			c.Header("Access-Control-Allow-Origin", origin)
+		}
 		c.Header("Access-Control-Allow-Headers", "Authorization, Content-Type")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Header("Access-Control-Max-Age", "86400")
